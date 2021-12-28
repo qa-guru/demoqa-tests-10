cb928f6fe985e55e192933a0cbd4fa482bceb226
svasenkov <aanher@gmail.com>
2021-12-28 22:19:31 +0300

    added negative tests

---
+++ src/test/java/guru/qa/tests/StudentsRegistrationFormNegativeTests.java
@@ -0,0 +1,31 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class StudentsRegistrationFormNegativeTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#submit").click();
+
+        $(".modal-content").shouldNot(exist);
+
+    }
+}
