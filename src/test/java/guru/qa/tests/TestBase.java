08f72a21089f3ebd45d357fd8fc3fcf9b088c2dc
svasenkov <aanher@gmail.com>
2021-12-28 21:35:21 +0300

    added TestBase

---
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -0,0 +1,13 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+}
